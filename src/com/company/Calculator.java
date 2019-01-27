08abd06d1a42de2edfd434f6d027b34329a2b03b
BeornPL <37457935+BeornPL@users.noreply.github.com>
2019-01-27 20:28:30 +0100

    Zadanie 4.6

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(5,3));
+    }
+}
